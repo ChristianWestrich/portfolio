faa1cb1f4c1f5f4478dd90f156de5b80dbd3ce3a
ChristianWestrich <christian.westrich@gmx.de>
2024-03-08 11:39:33 +0100

    finish css

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'christian.westrich@gmx.de';  
+            $recipient = 'kontakt@christian-westrich.de';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
